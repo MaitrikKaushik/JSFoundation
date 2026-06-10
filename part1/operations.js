b7b5e231f4e408825867c78adbf6e80ee40d3fcf
MaitrikKaushik <maitrik2006.kaushik.morni@gmail.com>
2026-06-10 22:05:10 +0530

    logic and operations

---
+++ part1/operations.js
@@ -0,0 +1,32 @@
+let addition = 4 + 5;
+
+let subtraction = 4 - 2;
+let mutli = 4 * 5;
+let divi = 4 / 2;
+let expo = 4 ** 2;
+let remainder = 9 % 2;
+
+let myScore = 101;
+myScore++;
+console.log(myScore);
+
+myScore--;
+
+console.log(mutli);
+console.log(myScore);
+
+//Comparison
+
+let num1 = 2;
+let num2 = 2;
+let num3 = 3;
+
+console.log(num1 == num2);
+console.log(num1 != num3);
+console.log(num3 > num2);
+console.log(num3 < num2);
+
+
+
+
+
